4f37153f13e4749d8070cfa2511fcb596ed97820
Kichul Kang <kichulkang@pal-10-184-4-0.itap.purdue.edu>
2022-01-22 18:48:07 -0500

    fixed mobile error, updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+//     plugins: ['react-native-reanimated/plugin']
+//   };
+// };
+
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
+
+// const pak = require('../package.json');
+
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: [
+//     [
+//       'module-resolver',
+//       {
+//         extensions: ['.tsx', '.ts', '.js', '.json'],
+//         ...
+//       }
+//     ],
+//     'react-native-reanimated/plugin' // PUT IT HERE
+//   ]
+// };
